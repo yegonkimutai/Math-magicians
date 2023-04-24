6f0b2231338277294963bf5bab2b8ca125f175fd
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-04-24 22:42:01 +0300

    Change files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More About React
+          Learn React
         </a>
       </header>
     </div>
