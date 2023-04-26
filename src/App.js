76d8f6a1614419d437e933e51661a0c4b6ea6a87
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-04-26 14:25:31 +0300

    Modify files

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-import { useState } from 'react';
-
 import './App.css';
-import Button from './components/Button';
 import Input from './components/Calculator';
 
-const App = () => {
-  const [text] = useState(0);
-  const [result] = useState('');
-
-  const btnColor = 'darkorange';
-
-  return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <Input text={text} result={result} />
-        <div className="btn-row">
-          <Button symbol="AC" />
-          <Button symbol="+/-" />
-          <Button symbol="%" />
-          <Button symbol="/" color={btnColor} />
-        </div>
-        <div className="btn-row">
-          <Button symbol="7" />
-          <Button symbol="8" />
-          <Button symbol="9" />
-          <Button symbol="*" color={btnColor} />
-        </div>
-        <div className="btn-row">
-          <Button symbol="4" />
-          <Button symbol="5" />
-          <Button symbol="6" />
-          <Button symbol="-" color={btnColor} />
-        </div>
-        <div className="btn-row">
-          <Button symbol="1" />
-          <Button symbol="2" />
-          <Button symbol="3" />
-          <Button symbol="+" color={btnColor} />
-        </div>
-        <div className="btn-row">
-          <div className="big-grid">
-            <Button symbol="0" />
-          </div>
-          <Button symbol="." />
-          <Button symbol="=" color={btnColor} />
-        </div>
-      </div>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <Input />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,35 @@
 import './Calculator.css';
-import PropTypes from 'prop-types';
 
-const Input = ({ text, result }) => (
-  <div className="input-wrapper">
-    <div className="result">
-      <h1>{result}</h1>
+const Input = () => (
+  <div className="calc-wrapper">
+    <div className="input-wrapper">
+      <h3 className="text">0</h3>
     </div>
-    <div className="text">
-      <h3>{text}</h3>
+    <div className="btn-row">
+      <button type="button" className="button">AC</button>
+      <button type="button" className="button">+/-</button>
+      <button type="button" className="button">%</button>
+      <button style={{ background: 'darkorange' }} type="button" className="button">/</button>
+      <button type="button" className="button">7</button>
+      <button type="button" className="button">8</button>
+      <button type="button" className="button">9</button>
+      <button style={{ background: 'darkorange' }} type="button" className="button">x</button>
+      <button type="button" className="button">4</button>
+      <button type="button" className="button">5</button>
+      <button type="button" className="button">6</button>
+      <button style={{ background: 'darkorange' }} type="button" className="button">-</button>
+      <button type="button" className="button">1</button>
+      <button type="button" className="button">2</button>
+      <button type="button" className="button">3</button>
+      <button style={{ background: 'darkorange' }} type="button" className="button">+</button>
+      <div className="big-grid">
+        <button type="button" className="button">0</button>
+      </div>
+      <button type="button" className="button">.</button>
+      <button style={{ background: 'darkorange' }} type="button" className="button">=</button>
     </div>
   </div>
-);
 
-Input.propTypes = {
-  text: PropTypes.number.isRequired,
-  result: PropTypes.number.isRequired,
-};
+);
 
 export default Input;
