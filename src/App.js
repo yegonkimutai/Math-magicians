912aa30bd486f2501f20beb310281a05a48444c6
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-04-27 12:27:13 +0300

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Input from './components/Calculator';
+import Api from './components/Api';
 
 const App = () => (
   <div className="App">
     <Input />
+    <Api />
   </div>
 );
 
