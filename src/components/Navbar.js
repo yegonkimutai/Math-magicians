e96db18d3f959c7e91039d0071248e26507a9e03
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-26 20:42:20 +0300

    :wrench:Fix linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,34 @@
+/* eslint-disable */
 import { useRef } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-  const navRef =  useRef();
+  const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.toggle('responsive-nav')
-  }
+    navRef.current.classList.toggle('responsive-nav');
+  };
 
   return (
     <header className="nav">
       <Link to="/" className="main-title">Math Magicians</Link>
-      <nav ref={navRef} className='nav-links'>
-      <ul className='home-links' onClick={showNavbar}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/quotes">Quotes</Link></li>
-      </ul>
-      <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-        <FaTimes />
+      <nav ref={navRef} className="nav-links">
+        <ul className="home-links" onClick={showNavbar}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/quotes">Quotes</Link></li>
+        </ul>
+        <button type="button" className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button type="button" className="nav-btn nav-open-btn" onClick={showNavbar}>
+        <FaBars />
       </button>
-    </nav>
-    <button className='nav-btn nav-open-btn' onClick={showNavbar}>
-      <FaBars />
-    </button>
     </header>
   );
-}
-
+};
 
 export default Navbar;
