9f8b3f0d409a12b1a9c76e346b88742ebdee161a
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-26 16:57:24 +0300

    :art:Make the components responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,34 @@
+import { useRef } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa'
 
-const Navbar = () => (
-  <nav className="nav">
-    <Link to="/" className="main-title">Math Magicians</Link>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/calculator">Calculator</Link></li>
-      <li><Link to="/quotes">Quotes</Link></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const navRef =  useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive-nav')
+  }
+
+  return (
+    <header className="nav">
+      <Link to="/" className="main-title">Math Magicians</Link>
+      <nav ref={navRef} className='nav-links'>
+      <ul className='home-links'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/quotes">Quotes</Link></li>
+      </ul>
+      <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+        <FaTimes />
+      </button>
+    </nav>
+    <button className='nav-btn nav-open-btn' onClick={showNavbar}>
+      <FaBars />
+    </button>
+    </header>
+  );
+}
+
 
 export default Navbar;
